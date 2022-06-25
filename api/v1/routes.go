f5b5e3658ce904df46cd160a9c19a7e206737f3a
pmoieni <pmoieni06@gmail.com>
2022-06-25 22:44:06 +0430

    initial commit

--- api/v1/routes.go
+++ api/v1/routes.go
@@ -2,10 +2,26 @@ package v1
 
 import (
 	"github.com/bellamy-labs/auth-api/api/v1/handlers"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/rs/zerolog/log"
 )
 
 func (s *Server) initRoutes() {
+	c := middleware.CORSConfig{
+		AllowOrigins:     []string{"*"},
+		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
+		AllowCredentials: true,
+	}
+	s.Handler.Use(middleware.CORSWithConfig(c))
+	s.Handler.Use(middleware.Secure())
+	// TODO: add CSRF protection
+	// s.Handler.Use(middleware.CSRF())
+	s.Handler.Use(middleware.BodyLimit("32M"))
+	// TODO: implement Redis for in memory store - https://echo.labstack.com/middleware/rate-limiter/
+	// TODO: add timeout functionality - https://echo.labstack.com/middleware/timeout/
+	s.Handler.Use(middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(20)))
+
 	apiGroup := s.Handler.Group("/api/v1")
 
 	authGroup := apiGroup.Group("/auth")
