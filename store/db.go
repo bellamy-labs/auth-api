722c830b8868b6a08b6a24322fa8ee973f4983f8
pmoieni <pmoieni06@gmail.com>
2022-06-25 22:05:16 +0430

    initial commit

--- store/db.go
+++ store/db.go
@@ -12,7 +12,7 @@ import (
 func InitDB() error {
 	client, err := ent.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=True", config.Cfg.DBUser, config.Cfg.DBPassword, config.Cfg.DBHost, config.Cfg.DBPort, config.Cfg.DBName))
 	if err != nil {
-		return errors.New("failed opening connection to sqlite: " + err.Error())
+		return errors.New("failed opening connection to mysql: " + err.Error())
 	}
 	defer client.Close()
 	// Run the auto migration tool.
